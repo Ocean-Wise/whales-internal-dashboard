e807552c891b6242aad9fc195f21b56727a05d71
Alex Mitchell <alex.mitchell@ocean.org>
2025-05-06 13:00:28 -0700

    Catagorize data pull between API and non-API

--- data-processing.R
+++ data-processing.R
@@ -10,7 +10,7 @@
 ## Currently not able to automate this data process, so to download the data, ensure to change the
 
 
-####~~~~~~~~~~~~~~~~~~~~~~Data Import~~~~~~~~~~~~~~~~~~~~~~~####
+####~~~~~~~~~~~~~~~~~~~~~~Data Import API ~~~~~~~~~~~~~~~~~~~~~~~####
 
 ## API query to get sightings data from the sightings database without downloading the whole dataset...
 
@@ -81,6 +81,8 @@
 #    !stringr::str_detect(reporter_email, "(?i)test")
 #  )
 
+
+####~~~~~~~~~~~~~~~~~~~~~~Data Import - non-API~~~~~~~~~~~~~~~~~~~~~~~####
  
 ## Get a list of files in the directory which we want to get the data from. It is important that older files are overwritten, not added. 
 file_list = list.files(paste0("C:/Users/", user, "/Ocean Wise Conservation Association/Whales Initiative - General/Ocean Wise Data/dashboard/"), full.names = T) %>% 
